a1e0949ad3195b0e5baf4ddbd08a3473bd6207e0
sokol7858 <36563791+sokol7858@users.noreply.github.com>
2018-04-14 02:20:16 +0300

    Repair time

--- task4.py
+++ task4.py
@@ -9,10 +9,18 @@ def task4(n,k):
     return A, B
 
 A, B=task4(100,1)
+
 x1=linalg.solve(A,B)
 
 x2=linalg.lu_solve(linalg.lu_factor(A),B,0)
 
 x3=np.dot(linalg.inv(A),B)
+
+print('solve_time')
+get_ipython().run_line_magic('timeit', 'linalg.solve(A,B)')
+print('lu_solve_time')
+get_ipython().run_line_magic('timeit', 'linalg.lu_solve(linalg.lu_factor(A),B,0)')
+print('dot_solve_time')
+get_ipython().run_line_magic('timeit', 'np.dot(linalg.inv(A),B)')
 
 
