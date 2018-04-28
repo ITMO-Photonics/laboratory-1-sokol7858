e4e11f495088fef2d08fd3ff33af5a42f6aa23a9
sokol7858 <36563791+sokol7858@users.noreply.github.com>
2018-04-28 03:16:33 +0300

    new

--- task4.py
+++ task4.py
@@ -1,27 +1,28 @@
 import numpy as np
-import scipy.linalg as linalg
+import scipy
+import timeit
+N = 50
+A = np.random.random((N,N))
+k = np.arange(N)
+def b(k,t):
+	return (k/(1+k*t))
 
-def task4(n,k):
-    A=np.random.random((n,n))
-    i,j=np.indices(A.shape)
-    t=np.linspace(0.,10.,100)
-    B=k/(1+k*t)
-    return A, B
+start = timeit.default_timer()
+for t in np.linspace(0,10,100):
+	x1 = scipy.linalg.solve(A,b(k,t))
+stop = timeit.default_timer()-start
+print(stop)
 
+start = timeit.default_timer()
+LU = scipy.linalg.lu_factor(A)
+for t in np.linspace(0,10,100):
+	x2 = scipy.linalg.lu_solve(LU,b(k,t))
+stop = timeit.default_timer()-start
+print(stop)
 
-A,B =task4(100,1)
-
-x1=linalg.solve(A,B)
-
-x2=linalg.lu_solve(linalg.lu_factor(A),B,0)
-
-x3=np.dot(linalg.inv(A),B)
-
-print('solve_time')
-get_ipython().run_line_magic('timeit', 'linalg.solve(A,B)')
-print('lu_solve_time')
-get_ipython().run_line_magic('timeit', 'linalg.lu_solve(linalg.lu_factor(A),B,0)')
-print('dot_solve_time')
-get_ipython().run_line_magic('timeit', 'np.dot(linalg.inv(A),B)')
-
-
+start = timeit.default_timer()
+Ainv = scipy.linalg.inv(A)
+for t in np.linspace(0,10,100):
+	x3 = np.dot(Ainv,b(k,t))
+	stop = timeit.default_timer()-start
+print(stop)
