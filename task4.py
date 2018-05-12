f0757fd93dcc22ab5460ea13caa35c86032ae1d5
sokol7858 <36563791+sokol7858@users.noreply.github.com>
2018-05-12 14:33:24 +0300

    done

--- task4.py
+++ task4.py
@@ -1,28 +1,30 @@
-import numpy as np
-import scipy
+﻿import numpy as np
+import scipy.linalg as linalg
 import timeit
 N = 50
 A = np.random.random((N,N))
 k = np.arange(N)
 def b(k,t):
-	return (k/(1+k*t))
+    return (k/(1+k*t))
 
 start = timeit.default_timer()
 for t in np.linspace(0,10,100):
-	x1 = scipy.linalg.solve(A,b(k,t))
+	x1 = linalg.solve(A,b(k,t))
 stop = timeit.default_timer()-start
 print(stop)
 
 start = timeit.default_timer()
-LU = scipy.linalg.lu_factor(A)
+LU = linalg.lu_factor(A)
 for t in np.linspace(0,10,100):
-	x2 = scipy.linalg.lu_solve(LU,b(k,t))
+	x2 = linalg.lu_solve(LU,b(k,t))
 stop = timeit.default_timer()-start
 print(stop)
 
 start = timeit.default_timer()
-Ainv = scipy.linalg.inv(A)
+Ainv = linalg.inv(A)
 for t in np.linspace(0,10,100):
 	x3 = np.dot(Ainv,b(k,t))
-	stop = timeit.default_timer()-start
+stop = timeit.default_timer()-start
 print(stop)
+
+
